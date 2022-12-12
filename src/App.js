83184915e4b4b83fc5eff43ee63181407ec1aeda
Comma Device <device@comma.ai>
2022-12-12 16:49:22 -0500

    start adding page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
+import { useEffect, useRef, useState } from "react"
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
 
 
 function Nav() {
@@ -14,6 +34,62 @@ function Nav() {
     
   )
 }
+function Slider() {
+  const url = "http://192.168.1.122:5000"
+  const vmin = 0
+  const vmax = 28
+  const [value, setValue] = useState(vmax)
+  //const int_loop = () => { sendv(value); }
+  //const onclick = (e) => {setValue(vmax); sendv(vmax) }
+  //const onchange = (e) => {const f = e.target.valueAsNumber; setValue(f); sendv(f)}
+  //const [done, setDone] = useState(true)
+  //const sendalways = (v) => {
+  //  try {
+  //    fetch(url + "/control/0/" + v, {cache: "no-store", mode:"no-cors", method: "GET", keepalive: true}).then(() => {
+  //      setDone(true)
+  //    }).catch((e) => {
+  //      setDone(true)
+  //    })
+  //  }
+  //  catch (e) {
+  //    setDone(true)
+  //  }
+  //}
+  //const sendv = (v) => {
+  //  // only send if done
+  //  if (!done) { console.log("wait"); return }
+  //  setDone(false)
+  //  sendalways(v)
+  //}
+  const c = "[&::-webkit-slider-thumb]:bg-nord8 [&::-webkit-slider-thumb]:w-12 [&::-webkit-slider-thumb]:h-12 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:[appearance:none] [&::-webkit-slider-thumb]:[-webkit-appearance:none] [-webkit-user-select:none] -translate-x-[134px] translate-y-[134px] -rotate-90 h-[32px] w-[300px] bg-gray-700 rounded-lg appearance-none cursor-pointer "
+  //useInterval(int_loop, 200)
+  useEffect(() => {
+    //sendalways(vmax)
+    console.log("reload")
+    return () => {
+      //sendalways(vmax)
+      console.log("unmount")
+    }
+  }, [])
+  return (
+    <>
+      <div className="h-[300px] w-[32px] mt-4">
+        <input type="range" step="1" defaultValue={vmax} min={vmin} max={vmax} className={c} />
+      </div>
+    </>
+  )
+  //return (
+  //  <>
+  //    <div className="h-[300px] w-[32px] mt-4">
+  //      <input type="range" step="1" onChange={onchange} value={value} min={vmin} max={vmax} className={c} />
+  //    </div>
+  //    <p className="text-xl font-bold mt-2">{value}</p>
+  //    <button onClick={onclick} className={"font-bold mt-4 w-64 h-24 rounded-3xl" + ((value == vmax) ? " bg-nord8 " : " bg-nord6 ")}>
+  //      <p className="text-nord1 text-2xl">reset</p>
+  //    </button>
+  //  </>
+  //)
+}
 function App() {
   const title = "manual set speed"
   return (
@@ -23,6 +99,7 @@ function App() {
       <h1 className="text-4xl">{title}</h1>
       <div className="flex flex-col items-center">
         <p>slider</p>
+        <Slider />
       </div>
     </div>
   </>
