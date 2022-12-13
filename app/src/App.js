62c0f7b6933c7f7fb3246a66901a2ebd454851c7
Comma Device <device@comma.ai>
2022-12-12 23:48:28 -0500

    ok but this shouldnt be nested lol

--- app/src/App.js
+++ app/src/App.js
@@ -19,21 +19,6 @@ function useInterval(callback, delay) {
   }, [delay]);
 }
 
-
-function Nav() {
-  const c = "bg-nord1 flex items-center justify-between"
-  const clinkalll = " text-nord5 hover:bg-nord5 hover:text-nord0 py-1 px-2 "
-  const clinkhome = " text-nord5 bg-nord10  py-1 px-2 "
-  return (
-    <div className={c}>
-      <div className="flex items-center">
-      <p className={clinkhome}>Home</p>
-      <p className={clinkalll}>Set Speed</p>
-      </div>
-    </div>
-    
-  )
-}
 function Slider() {
   const url = "http://192.168.1.122:5000"
   const vmin = 0
@@ -87,7 +72,6 @@ function App() {
   const title = "manual set speed"
   return (
   <>
-    <Nav />
     <div className="text-nord5 w-full mt-3 flex flex-col items-center justify-between">
       <h1 className="text-4xl">{title}</h1>
       <div className="flex flex-col items-center">
--- app/src/routes/root.jsx
+++ app/src/routes/root.jsx
@@ -1,13 +1,14 @@
-import { Link, Outlet } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom"
 function Nav() {
   const c = "bg-nord1 flex items-center justify-between"
+  const clink = (active) => { return active ? clinkhome : clinkalll}
   const clinkalll = " text-nord5 hover:bg-nord5 hover:text-nord0 py-1 px-2 "
   const clinkhome = " text-nord5 bg-nord10  py-1 px-2 "
   return (
     <div className={c}>
       <div className="flex items-center">
-      <p className={clinkhome}>Home</p>
-      <p className={clinkalll}>Set Speed</p>
+      <NavLink to="/" className={clink}>Home</NavLink>
+      <NavLink to="/setspeed" className={clink}>Set Speed</NavLink>
       </div>
     </div>
   )
