5682dae99ca424dc9dd5c0ce20494377b82f2977
Comma Device <device@comma.ai>
2022-12-12 20:54:41 -0500

    basic shmem setup

--- shminject.py
+++ shminject.py
@@ -5,22 +5,32 @@
 # use this instead of file read/write to share setspeed with op
 
 from multiprocessing import shared_memory
+import atexit
 
-name="abcd"
-shm = shared_memory.SharedMemory(name=name, create=True, size=1)
-shmb = shared_memory.SharedMemory(shm.name)
-type(shm.buf)
-buf = shm.buf
-buf[0] = 4
-len(buf)
-print(buf)
-print(buf[0])
-print(dir(buf))
-a = buf
-print(a)
-#buf[:4] = bytearray([22, 33, 44, 55])
+class Mem:
+  __mem = None
+  name = "fff"
+  size = 1
+  vinit = 28 # vmax
+  def set(self, v):
+    #buf[:4] = bytearray([22, 33, 44, 55])
+    self.__mem.buf[0] = v
+  def get(self):
+    return self.__mem.buf[0]
+  def __create_or_connect(self):
+    self.__mem = shared_memory.SharedMemory(name=self.name, create=True, size=self.size)
+  def __cleanup(self):
+    self.__mem.close()
+    if self.__shouldunlink:
+      self.__mem.unlink()
+  def __init__(self, autounlink=False):
+    self.__shouldunlink = autounlink
+    self.__create_or_connect()
+    self.set(self.vinit)
+    atexit.register(self.__cleanup)
 
+def main():
+  mem = Mem(autounlink=True)
+  mem.set(0)
 
-shm.close()
-shmb.close()
-shm.unlink()
+main()
