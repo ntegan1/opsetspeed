880464e1559ab9b065560d66d139f1a705a65f54
ntegan1 <ntegan1@gmail.com>
2022-12-13 18:29:46 -0500

    try autobuild

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
+import os
 import time
 import threading
 from flask import Flask, send_from_directory, Response
 from opsetspeed.shminject import Mem
 
-# TODO: if no build dir, run make
-
 build_dir="./build"
+if not os.path.exists(build_dir):
+  os.system("make -C /data/openpilot/opsetspeed")
 static_dir=build_dir + "/static"
 allowed_build = ["asset-manifest.json", "favicon.ico", "logo192.png", "logo512.png", "robots.txt"]
 app = Flask(__name__, static_folder=static_dir)
