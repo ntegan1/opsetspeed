11f49d6b50e4d077e8cf2c28c3fb6a25ce4761ab
ntegan1 <ntegan1@gmail.com>
2022-12-12 23:25:24 -0500

    4reactrouterandaction (#6)
    * github actions info links
    
    quickstart
    https://docs.github.com/en/actions/quickstart
    
    start with nodejs
    https://docs.github.com/en/actions/automating-builds-and-tests/building-and-testing-nodejs
    
    artifact build stuff
    https://docs.github.com/en/actions/using-workflows/storing-workflow-data-as-artifacts
    
    * action demo
    
    * try node
    
    * cd to node app
    
    * trythis
    
    * install
    
    * try archive
    
    * maybe only archives dirs
    
    * hmmm
    
    * ok artifacts but doesnt safe the nested build dirhmmm good enough
    
    * basic router
    
    * routes dir
    
    * error page
    
    * guess should try nested routes
    
    * second route
    
    * nest it
    
    * Link client side route
    
    Co-authored-by: Comma Device <device@comma.ai>

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Root from './routes/root';
+import Contact from './routes/contact';
+import ErrorPage from './route-error-page'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// finished some of the tut
+//https://reactrouter.com/en/main/start/tutorial
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+    //<App />
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
